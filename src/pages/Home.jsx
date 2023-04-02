99d7a80bccc95436113dd3aa1c6e75525307e670
Madhavendra Rathore <madhavendrarathore@Madhavendras-MacBook-Air.local>
2023-04-02 14:01:19 +0530

    Main to deplo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,10 +10,10 @@ const Homepage = () => {
         <div className='w-[99vw] min-h-[30vw] h-min flex my-20 justify-center px-[10vw]  '>
             <Intro />
         </div>
-        <div className='w-[99vw]   flex flex-col'>
+        <div className='w-[99vw] flex flex-col'>
             <Domains />
         </div>
-        <div className='w-[99vw] flex flex-col my-5'>
+        <div className='w-[99vw]  flex flex-col my-5'>
           <OurEvents />
         </div>
         <div className='w-[99vw] flex flex-col my-5'>
